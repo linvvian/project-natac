c9878fd4d58cf3c1b6a7404ff2da815a99dd28c7
linvvian <lin.vvian@gmail.com>
2017-06-20 10:58:40 -0400

    adds div in hexagon

---
+++ catan_client/src/player.js
@@ -0,0 +1,14 @@
+class Player {
+  constructor(name){
+    this.settlements = []
+    this.roads = []
+    this.name = name || `Player`
+  }
+
+  render(){
+    return `<h1>${this.name}</h1>`
+  }
+}
+
+const me = new Player('me')
+document.getElementById('player1-corner').innerHTML = me.render()
